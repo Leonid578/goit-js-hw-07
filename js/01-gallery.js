2f585d093f139053a37f432067a79facb05ccf62
Leonid578 <Leonid.Ivanov187@gmail.com>
2021-12-18 04:22:57 +0200

    3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryConteiner  = document.querySelector('.gallery')
+
+const photoMarkup = createGalleryy(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('beforeend', photoMarkup);
+
+
+function createGalleryy(galleryItems) {
+return galleryItems.map(({preview, original, description}) => 
+  { return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `
+  }).join('')
+};
+
+galleryConteiner.addEventListener( "click", onGalleryClick );
+
+function onGalleryClick(evt) {
+    evt.preventDefault()
+    const imgItem = evt.target.classList.contains('gallery__image');
+    if (!imgItem) {
+      return
+    }
+    console.log(evt.target.dataset.source);
+  
+    const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="1400" height="900">
+  `)
+  
+    instance.show()
+    
+    document.addEventListener("keydown", evt => {
+      if (evt.code === "Escape") {
+      instance.close(() => console.log('lightbox not visible anymore'))
+    }
+  });
+  }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+class Gallerey1 {
+    constructor (preview, original, description) {
+        this.preview = preview
+        this.original = original
+        this.description = description
+   }
+}
+
+// получим родителя для нашей галереии
+const gallerey = document.querySelector( '.gallery' )
+
+
+// функция динамической разметки передаем в нее любой  массив изображений и рендерим разметку
+const html = (gallery) => {
+    return gallery.map( ( img ) => {
+        const imgEl = new Gallerey1( img.preview, img.original, img.description )
+        const htmlEl = `<a class="gallery__item" href="${imgEl.original}">
+            <img class="gallery__image" src="${imgEl.preview}" title ="${imgEl.description}" alt="${imgEl.description}" />
+            </a>`
+       return htmlEl
+    })
+}
+      
+// повесим нашу разметку на сайт
+gallerey.insertAdjacentHTML("beforeend", html(galleryItems).join(""))
+
+//  в аргументы передаем настройки для новой галереи
+let gallerySet = new SimpleLightbox( '.gallery a', { captionPosition: "bottom", captionDelay: 250 } );
+
+gallerySet.on( 'show.simplelightbox', function () {
+    console.log( "Получаеться прослушивать клики даже ненужно, все уже есть под капотом" )
+} );
